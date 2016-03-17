b581ffd4e1e1aa939ba8b163cd98cece4dd01b5e
Nicolas Ganivet <nicolas@cividesk.com>
2016-03-17 00:51:36 -0600

    Decapsulating output of CRM_Utils_Mail::formatRFC822Email()

--- Mail/Sparkpost.php
+++ Mail/Sparkpost.php
@@ -65,11 +65,30 @@ class Mail_Sparkpost extends Mail {
       $recipients = array($recipients);
     }
     foreach ($recipients as $recipient) {
-      $request_body['recipients'][] = array(
-        'address' => array(
-          'email' => $recipient,
-        )
-      );
+      // Format is: a plain email address
+      if (substr($recipient, -1) != '>') {
+        $request_body['recipients'][] = array(
+          'address' => array(
+            'email' => $recipient,
+          )
+        );
+      } else {
+        // Address is supposed to be RFC822 compliant, but since
+        // CRM_Utils_Mail::formatRFC822Email() is doing a shitty job
+        // by not using quotes, we cannot use a regexp to decapsulate
+        $pos = strrpos($recipient, '<');
+        $email = substr($recipient, $pos+1, -1);
+        $name = trim(substr($recipient, 0, $pos));
+        if (substr($name, 0, 1) == '"') {
+          $name = substr($name, 0, -1);
+        }
+        $request_body['recipients'][] = array(
+          'address' => array(
+            'name' => $name,
+            'email' => $email,
+          )
+        );
+      }
     }
 
     // Construct the rfc822 encapsulated email

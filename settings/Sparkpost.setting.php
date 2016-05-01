d774936936fa99457dbb9ada69b62a0b43f3ee46
Nicolas Ganivet <nicolas@cividesk.com>
2016-05-01 11:37:36 -0600

    Adjust settings for CiviCRM API

--- settings/Sparkpost.setting.php
+++ settings/Sparkpost.setting.php
@@ -4,17 +4,30 @@
  */
 
 return array(
-  'apiKey' => array(
-    'group_name' => 'SparkPost Settings',
+  'sparkpost_apiKey' => array(
+    'group_name' => CRM_Sparkpost::SPARKPOST_EXTENSION_SETTINGS,
     'group' => 'com.cividesk.email.sparkpost',
-    'name' => 'apiKey',
+    'name' => 'sparkpost_apiKey',
     'type' => 'String',
-    'html_type' => 'Text',
+    'html_type' => 'password',
     'default' => null,
-    'add' => '4.3',
+    'add' => '4.4',
     'is_domain' => 1,
     'is_contact' => 0,
     'description' => 'SparkPost REST API key',
     'help_text' => 'You can create API keys at: https://app.sparkpost.com/account/credentials',
-  )
+  ),
+  'sparkpost_useBackupMailer' => array(
+    'group_name' => CRM_Sparkpost::SPARKPOST_EXTENSION_SETTINGS,
+    'group' => 'com.cividesk.email.sparkpost',
+    'name' => 'sparkpost_useBackupMailer',
+    'type' => 'Boolean',
+    'html_type' => 'radio',
+    'default' => FALSE,
+    'add' => '4.4',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'Use backup mailer?',
+    'help_text' => 'The backup mailer will be used if Sparkpost cannot send emails (unverified sending domain, sending limits exceeded, ...).',
+  ),
 );
